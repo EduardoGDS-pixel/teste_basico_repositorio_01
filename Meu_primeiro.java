50ef199af02df56ac3c66e7dbec7faea92861119
Eduardo Geffert da Silva <gefferteduardo@gmail.com>
2025-08-29 21:33:23 -0300

    ola
    Oi

---
+++ Meu_primeiro.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Meu_primeiro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Calculadora Simples");
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o operador (+, -, *, /): ");
+        char operador = scanner.next().charAt(0);
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        double resultado;
+        switch (operador) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Erro: Divisão por zero!");
+                    scanner.close();
+                    return;
+                }
+                resultado = num1 / num2;
+                break;
+            default:
+                System.out.println("Operador inválido!");
+                scanner.close();
+                return;
+        }
+
+        System.out.println("Resultado: " + resultado);
+        scanner.close();
+    }
+}
